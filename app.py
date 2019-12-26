9d7050630d4385789e1be40a72cd4a6196a2c07b
peggyppp <47789235+peggyppp@users.noreply.github.com>
2019-12-26 21:04:39 +0800

    try again

---
+++ app.py
@@ -0,0 +1,15 @@
+import os, requests
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "hello"
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
